b209b9c1caef008abe5a9d686046a685a26a1695
Tadhg <tadhgsmboyle@gmail.com>
2020-03-09 23:02:57 -0700

    alerts automatically dismiss

--- resources/views/includes/messages.blade.php
+++ resources/views/includes/messages.blade.php
@@ -1,5 +1,5 @@
 @if (\Session::has('success'))
-<div class="alert alert-success alert-dismissible">
+<div class="alert alert-success alert-dismissible fade show">
     <span>{!! \Session::get('success') !!}</span>
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
@@ -7,10 +7,19 @@
 </div>
 @endif
 @if (\Session::has('error'))
-<div class="alert alert-danger alert-dismissable">
+<div class="alert alert-danger alert-dismissable fade show">
     <span>{!! \Session::get('error') !!}</span>
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
 </div>
 @endif
+<script>
+    $(document).ready(function() {
+        setTimeout(
+            function() {
+                $(".alert").alert('close')
+                console.log('closing alert');
+            }, 2000);
+    });
+</script>
