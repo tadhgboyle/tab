22f41377506f3d8c454b2714fbb165645bf7aeae
Tadhg <tadhgsmboyle@gmail.com>
2020-05-25 15:18:57 -0700

    popularProducts graph use lookBack

--- app/Products.php
+++ app/Products.php
@@ -4,14 +4,15 @@ namespace App;
 
 use App\Http\Controllers\OrderController;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Products extends Model
 {
-    public static function findSold($product) {
+    public static function findSold($product, $lookBack) {
 
         $sold = 0;
 
-        foreach (Transactions::all() as $transaction) {
+        foreach (Transactions::where('created_at', '>=', Carbon::now()->subDays($lookBack)->toDateTimeString())->get() as $transaction) {
             foreach(explode(", ", $transaction->products) as $transaction_product) {
                if (OrderController::deserializeProduct($transaction_product)['id'] == $product) $sold++;
             }
