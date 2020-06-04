e473fdcded01de737dbe2a1c466ee836ac75ad1c
Tadhg <tadhgsmboyle@gmail.com>
2020-06-03 22:28:00 -0700

    beginning work on stock/inventory
    now product editing/creation uses the same view as well

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,5 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
+        aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse">
@@ -7,57 +8,61 @@
             <?php
             if (Auth::check()) {
             ?>
-                <li class="nav-item">
-                    <a href="/" class="nav-link">Cashier</a>
-                </li>
-                <?php
+            <li class="nav-item">
+                <a href="/" class="nav-link">Cashier</a>
+            </li>
+            <?php
                 if (Auth::user()->role == "administrator") {
                 ?>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Users
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="/users">List</a>
-                            <a class="dropdown-item" href="/users/new">Create</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Products
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="/products">List</a>
-                            <a class="dropdown-item" href="/products/new">Create</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/orders" class="nav-link">Orders</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Statistics
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="/statistics">Text</a>
-                            <a class="dropdown-item" href="/statistics/graphs">Graphs</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/settings" class="nav-link">Settings</a>
-                    </li>
-                <?php
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Users
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="/users">List</a>
+                    <a class="dropdown-item" href="/users/new">Create</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Products
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="/products">List</a>
+                    <a class="dropdown-item" href="/products/new">Create</a>
+                    <a class="dropdown-item" href="/products/receive">Receive</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a href="/orders" class="nav-link">Orders</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Statistics
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="/statistics">Text</a>
+                    <a class="dropdown-item" href="/statistics/graphs">Graphs</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a href="/settings" class="nav-link">Settings</a>
+            </li>
+            <?php
                 }
                 ?>
-                <li class="nav-item">
-                    <a href="/logout" class="nav-link">Logout</a>
-                </li>
+            <li class="nav-item">
+                <a href="/logout" class="nav-link">Logout</a>
+            </li>
             <?php
             } else {
             ?>
-                <li class="nav-item">
-                    <a href="/login" class="nav-link">Login</a>
-                </li>
+            <li class="nav-item">
+                <a href="/login" class="nav-link">Login</a>
+            </li>
             <?php } ?>
         </ul>
     </div>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -23,8 +23,9 @@
         @include('includes.footer')
 
     </div>
-</body>
 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
 
 </html>
