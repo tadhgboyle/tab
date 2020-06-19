b4bb4e9f4db90fc78226a1295a3e9d4bec99ba8b
Tadhg <tadhgsmboyle@gmail.com>
2020-06-19 16:15:19 -0700

    Add simple loader for cashier page + functionify sidebar

--- public/item-sidebar.js
+++ public/item-sidebar.js
@@ -1,59 +1,41 @@
-// This is blackmagic code that (somehow) handles the live sidebar
 const checked = [];
 let total_gst = total_pst = total_tax_percent = total_price = 0;
 const current_gst = parseFloat(document.getElementById('current_gst').value).toFixed(2);
 const current_pst = parseFloat(document.getElementById('current_pst').value).toFixed(2);
-
 const purchaser_balance = parseFloat(document.getElementById('purchaser_balance').value).toFixed(2);
 
-$("#gst").html('GST: $' + total_gst.toFixed(2));
-$("#pst").html('PST: $' + total_pst.toFixed(2));
-$("#total_price").html('Total Price: $' + total_price.toFixed(2));
-$("#remaining_balance").html('Remaining Balance: $' + (purchaser_balance - total_price).toFixed(2));
-
 // Edge Case: Page was reloaded with things selected
-// TODO: very inefficient code. will fix when fully workin'
-// TODO: Loading spinner while this page loads
 $(document).ready(function () {
+    updateInfo();
     $('#order').find(':input').each(function () {
         const input = document.getElementById($(this).attr('id'));
-        if (input != null) {
-            if ($(input).attr('type') == 'checkbox') {
-                if (input.checked) {
-                    const quantity = parseFloat(document.getElementById('quantity[' + input.value + ']').value);
-                    const info = document.getElementsByName('product[' + input.value + ']')[0].id;
-                    const price = parseFloat(info.split('$')[1]);
-                    const pst_id = document.getElementById('pst[' + input.value + ']').value;
+        if (input != null && $(input).attr('type') == 'checkbox' && input.checked) {
+            const quantity = parseFloat(document.getElementById('quantity[' + input.value + ']').value);
+            if (quantity > 0) {
+                const info = document.getElementsByName('product[' + input.value + ']')[0].id;
+                const price = parseFloat(info.split('$')[1]);
+                const pst_id = document.getElementById('pst[' + input.value + ']').value;
 
-                    if (pst_id == 1) {
-                        // I dont know why we need parseFloat(), but shit breaks without it
-                        total_tax_percent += (parseFloat(current_pst) + parseFloat(current_gst) - 1);
-                        total_pst += price * quantity * current_pst - price * quantity;
-                    } else total_tax_percent += current_gst;
-                    total_gst += price * quantity * current_gst - price * quantity;
-                    total_price += price * quantity * total_tax_percent;
+                if (pst_id == 1) {
+                    // I dont know why we need parseFloat(), but shit breaks without it
+                    total_tax_percent += (parseFloat(current_pst) + parseFloat(current_gst) - 1);
+                    total_pst += price * quantity * current_pst - price * quantity;
+                } else total_tax_percent += current_gst;
+                total_gst += price * quantity * current_gst - price * quantity;
+                total_price += price * quantity * total_tax_percent;
 
-                    checked.push(info + ' (x' + quantity + ')<br>');
+                checked.push(info + ' (x' + quantity + ')<br>');
 
-                    $("#items").html(checked);
-                    $("#gst").html('GST: $' + total_gst.toFixed(2));
-                    $("#pst").html('PST: $' + total_pst.toFixed(2))
-
-                    if (total_price > purchaser_balance) {
-                        // Disable things if they do not have enough money to proceed
-                        $('.disableable').prop('disabled', true);
-                        $("#total_price").html('<span style="color:red">Total Price: $' + total_price.toFixed(2) + '</span>');
-                        $("#remaining_balance").html('<span style="color:red">Remaining Balance: $' + (purchaser_balance - total_price).toFixed(2) + '</span>');
-                    } else {
-                        $('.disableable').prop('disabled', false);
-                        $("#total_price").html('Total Price: $' + total_price.toFixed(2));
-                        $("#remaining_balance").html('Remaining Balance: $' + (purchaser_balance - total_price).toFixed(2));
-                    }
-                }
-            }
+                updateInfo();
+            } else input.prop('checked', false);
         }
     });
 })
+
+$(window).on('load', function () {
+    $('#loading').hide();
+    $('#cashier').show();
+});
 
 // Handle clicks on items
 $('.clickable').click(function () {
@@ -88,13 +70,21 @@ $('.clickable').click(function () {
             if (index >= 0) {
                 checked.splice(index, 1);
                 total_gst -= (current_price * quantity) * current_gst - current_price * quantity;
-                console.log('total:' + total_price);
                 // Janky fix for after page reloading + unselecting -> negative total price
                 total_price -= (current_price * quantity) * total_tax_percent;
                 if (total_price < 0) total_price = 0;
             }
         }
-    }
+    } else $(this).prop('checked', false);
+
+    updateInfo();
+
+    // This is needed for some silly reason
+    total_tax_percent = 0;
+});
+
+
+function updateInfo() {
 
     $("#items").html(checked);
     $("#gst").html('GST: $' + total_gst.toFixed(2));
@@ -110,9 +100,7 @@ $('.clickable').click(function () {
         $("#total_price").html('Total Price: $' + total_price.toFixed(2));
         $("#remaining_balance").html('Remaining Balance: $' + (purchaser_balance - total_price).toFixed(2));
     }
-    // This is needed for some silly reason
-    total_tax_percent = 0;
-});
+}
 
 // Removes the "disabled" attribute from quantity fields. 
 // Without this, no selected items are sent to the controller
