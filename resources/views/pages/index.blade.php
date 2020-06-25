5a0e5e5f85c9a79b0c9e27d78c3063d98c14a8a2
Tadhg <tadhgsmboyle@gmail.com>
2020-06-24 22:57:36 -0700

    Add setting for cashiers to not be able to ring thru themselves

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,27 +4,33 @@
 <?php
 
 use App\User;
+use App\Http\Controllers\SettingsController;
+use Illuminate\Support\Facades\Auth;
 ?>
 <div class="row">
     <div class="col-md-3"></div>
     <div class="col-md-6">
-    @include('includes.messages')
+        @include('includes.messages')
         <table id="user_list">
             <thead>
                 <th>Full Name</th>
                 <th>Balance</th>
             </thead>
             <tbody>
-                @foreach(User::where('deleted', false)->get() as $result)
-                <tr>
-                    <td class="table-text">
-                        <div><a href="orders/{{ $result->id }}">{{ $result->full_name }}</a></div>
-                    </td>
-                    <td class="table-text">
-                        <div>${{ number_format($result->balance, 2) }}</div>
-                    </td>
-                </tr>
-                @endforeach
+                @foreach(
+                User::where('deleted', false)->when(!SettingsController::getSelfPurchases(), function ($query) {
+                if (Auth::user()->role != 'administrator') {
+                return $query->where('id', '<>', Auth::user()->id);
+                    }})->get() as $result)
+                    <tr>
+                        <td class="table-text">
+                            <div><a href="orders/{{ $result->id }}">{{ $result->full_name }}</a></div>
+                        </td>
+                        <td class="table-text">
+                            <div>${{ number_format($result->balance, 2) }}</div>
+                        </td>
+                    </tr>
+                    @endforeach
             </tbody>
         </table>
     </div>
