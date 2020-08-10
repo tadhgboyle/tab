6ae6b8671d573bb3c0dd48c45a135cba236b9d60
Tadhg <tadhgsmboyle@gmail.com>
2020-08-10 10:54:12 -0700

    convert products form to Bulma

--- resources/views/pages/products/form.blade.php
+++ resources/views/pages/products/form.blade.php
@@ -6,7 +6,8 @@ $product = Products::find(request()->route('id'));
 @endphp
 @extends('layouts.default')
 @section('content')
-<h2><strong>{{ is_null($product) ? 'Create' : 'Edit' }} Product</strong></h2>
+<h2 class="title has-text-weight-bold">{{ is_null($product) ? 'Create' : 'Edit' }} Product</h2>
+@if(!is_null($product)) <p><strong>Product:</strong> {{ $product->name }}</p> @endif
 <style>
     select:required:invalid {
         color: gray;
@@ -20,9 +21,10 @@ $product = Products::find(request()->route('id'));
         color: black;
     }
 </style>
-<div class="row">
-    <div class="col-md-2"></div>
-    <div class="col-md-4">
+<div class="columns">
+    <div class="column is-1"></div>
+
+    <div class="column is-5">
         @include('includes.messages')
         <form action="/products/{{ is_null($product) ? 'new' : 'edit' }}" id="product_form" method="POST"
             class="form-horizontal">
@@ -30,78 +32,114 @@ $product = Products::find(request()->route('id'));
             <input type="hidden" name="id" value="{{ Auth::user()->id }}">
             <input type="hidden" name="product_id" id="product_id" value="{{ $product->id ?? null }}">
 
-            <span>Name<sup style="color: red">*</sup></span>
-            <input type="text" name="name" class="form-control" placeholder="Name"
-                value="{{ $product->name ?? old('name') }}">
-            <br>
-            <span>Price<sup style="color: red">*</sup></span>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <div class="input-group-text">$</div>
+            <div class="field">
+                <label class="label">Name<sup style="color: red">*</sup></label>
+                <div class="control">
+                    <input type="text" name="name" class="input" placeholder="Name" value="{{ $product->name ?? old('name') }}">
                 </div>
-                <input type="number" step="0.01" name="price" class="form-control" placeholder="Price"
-                    value="{{ isset($product->price) ? number_format($product->price, 2) : number_format(old('price'), 2) }}">
             </div>
-            <br>
 
-            <span>PST</span>
-            <input type="checkbox" name="pst" {{ (isset($product->pst) && $product->pst) || old('pst') ? 'checked' : '' }}>
-            <br>
-            <br>
-            <span>Category<sup style="color: red">*</sup></span>
-            <select name="category" class="form-control">
-                {{!! !isset($product->category) ? "<option value=\"\" disabled selected>Select Category...</option>" : '' !!}}
-                @foreach(SettingsController::getCategories() as $category)
-                    <option value="{{ $category->value }}"
-                        {{ (!is_null($product) && $product->category == $category->value) || old('category') == $category->value  ? 'selected' : '' }}>
-                        {{ ucfirst($category->value) }}
-                    </option>
-                @endforeach
-            </select>
-            <br>
+            <div class="field">
+                <label class="label">Price<sup style="color: red">*</sup></label>
+                <div class="control has-icons-left">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-dollar-sign"></i>
+                    </span>
+                    <input type="number" step="0.01" name="price" class="input" value="{{ isset($product->price) ? number_format($product->price, 2) : number_format(old('price'), 2) }}">
+                </div>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <label class="checkbox label">
+                        PST
+                        <input type="checkbox" name="pst" {{ (isset($product->pst) && $product->pst) || old('pst') ? 'checked' : '' }}>
+                    </label>
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">Category<sup style="color: red">*</sup></label>
+                <div class="control">
+                    <div class="select">
+                        <select name="category">
+                            {{!! !isset($product->category) ? "<option value=\"\" disabled selected>Select Category...</option>" : '' !!}}
+                            @foreach(SettingsController::getCategories() as $category)
+                                <option value="{{ $category->value }}"
+                                    {{ (!is_null($product) && $product->category == $category->value) || old('category') == $category->value  ? 'selected' : '' }}>
+                                    {{ ucfirst($category->value) }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+            </div>
     </div>
-    <div class="col-md-4">
+    <div class="column is-4">
 
-        <span>Stock</span>
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <div class="input-group-text">#</div>
+        <div class="field">
+            <label class="label">Stock</label>
+            <div class="control has-icons-left">
+                <span class="icon is-small is-left">
+                    <i class="fas fa-hashtag"></i>
+                </span>
+                <input type="number" step="1" name="stock" class="input unlimited_stock_attr" placeholder="Stock" value="{{ $product->stock ?? old('stock') }}" readonly>
             </div>
-            <input type="number" step="1" name="stock" class="form-control unlimited_stock_attr" placeholder="Stock"
-                value="{{ $product->stock ?? old('stock') }}" readonly>
         </div>
-        <br>
-        <span>Box Size</span>
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <div class="input-group-text">#</div>
-            </div>
-            <input type="number" step="1" name="box_size" class="form-control unlimited_stock_attr"
-                placeholder="Box Size" value="{{ $product->box_size ?? old('stock') }}" readonly>
-        </div>
-        <br>
 
-        <span>Unlimited Stock</span>
-        <input type="checkbox" name="unlimited_stock"
-            {{ (isset($product->unlimited_stock) && $product->unlimited_stock) || old('unlimited_stock') ? 'checked' : '' }}>
-        &nbsp;
-        <span>Stock Override</span>
-        <input type="checkbox" name="stock_override"
-            {{ (isset($product->stock_override) && $product->stock_override) || old('stock_override') ? 'checked' : '' }}>
+        <div class="field">
+            <label class="label">Box Size</label>
+            <div class="control has-icons-left">
+                <span class="icon is-small is-left">
+                    <i class="fas fa-hashtag"></i>
+                </span>
+                <input type="number" step="1" name="box_size" class="input unlimited_stock_attr" placeholder="Box Size" value="{{ $product->box_size ?? old('stock') }}" readonly>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <label class="checkbox label">
+                    Unlimited Stock
+                    <input type="checkbox" name="unlimited_stock" {{ (isset($product->unlimited_stock) && $product->unlimited_stock) || old('unlimited_stock') ? 'checked' : '' }}>
+                </label>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <label class="checkbox label">
+                    Stock Override
+                    <input type="checkbox" name="stock_override" {{ (isset($product->stock_override) && $product->stock_override) || old('stock_override') ? 'checked' : '' }}>
+                </label>
+            </div>
+        </div>
 
         </form>
     </div>
-    <div class="col-md-2">
+    <div class="column is-2">
         <form>
-            <button type="submit" form="product_form"
-                class="button is-success">{{ is_null($product) ? 'Create' : 'Edit' }} Product</button>
+            <div class="control">
+                <button class="button is-success" type="submit" form="product_form">
+                    <span class="icon is-small">
+                        <i class="fas fa-check"></i>
+                    </span>
+                    <span>Submit</span>
+                </button>
+            </div>
         </form>
         <br>
         @if(!is_null($product))
-            <form>
-                <a href="javascript:;" data-toggle="modal" onclick="deleteData()" data-target="#DeleteModal"
-                    class="button is-danger">Delete</a>
-            </form>
+            <div class="control">
+                <form>
+                    <a class="button is-danger is-outlined" href="javascript:;" data-toggle="modal" onclick="deleteData()" data-target="#DeleteModal">
+                        <span>Delete</span>
+                        <span class="icon is-small">
+                            <i class="fas fa-times"></i>
+                        </span>
+                    </a>
+                </form>
+            </div>
         @endif
     </div>
 </div>
