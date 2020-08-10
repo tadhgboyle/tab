d311ef25b8ce5556ea8a121b1dc6d20926ca82f9
Tadhg <tadhgsmboyle@gmail.com>
2020-08-10 10:20:59 -0700

    convert user form to Bulma
    todo: modals and other forms

--- resources/views/pages/users/form.blade.php
+++ resources/views/pages/users/form.blade.php
@@ -8,89 +8,136 @@ if (!is_null($user) && $user->deleted) return redirect('/users')->with('error', 
 @endphp
 @extends('layouts.default')
 @section('content')
-<h2><strong>{{ is_null($user) ? 'Create' : 'Edit' }} User</strong></h2>
+<h2 class="title has-text-weight-bold">{{ is_null($user) ? 'Create' : 'Edit' }} User</h2>
 @if(!is_null($user)) <p>User: {{ $user->full_name }} <a href="/users/info/{{ $user->id }}">(Info)</a></p> @endif
-<div class="row">
-    <div class="col-md-2"></div>
-    <div class="col-md-4">
+<div class="columns">
+    <div class="column is-1"></div>
+
+    <div class="column is-5">
         <form action="/users/{{ is_null($user) ? 'new' : 'edit' }}" id="user_form" method="POST">
             @csrf
             <input type="hidden" name="id" id="user_id" value="{{ request()->route('id') }}">
-            <span>Full Name<sup style="color: red">*</sup></span>
-            <input type="text" name="full_name" class="form-control" placeholder="Full Name"
-                value="{{ $user->full_name ?? old('full_name') }}">
 
-            <span>Username</span>
-            <input type="text" name="username" class="form-control" placeholder="Username (Optional)"
-                value="{{ $user->username ?? old('username') }}">
-
-            <span>Balance</span>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <div class="input-group-text">$</div>
+            <div class="field">
+                <label class="label">Full Name</label>
+                <div class="control">
+                    <input type="text" name="full_name" class="input" placeholder="Full Name" value="{{ $user->full_name ?? old('full_name') }}">
                 </div>
-                <input type="number" step="0.01" name="balance" class="form-control" placeholder="Balance"
-                    value="{{ isset($user->balance) ? number_format($user->balance, 2) : number_format(old('balance'), 2) }}">
+            </div>
+            
+            <div class="field">
+                <label class="label">Username</label>
+                <div class="control has-icons-left">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-user"></i>
+                    </span>
+                    <input type="text" name="username" class="input" placeholder="Username (Optional)" value="{{ $user->username ?? old('username') }}">
+                </div>
             </div>
 
-            <label for="camper">Camper</label>
-            <input type="radio" name="role" value="camper" @if((isset($user->role) && $user->role ==
-            "camper")||
-            old('role') == "camper") checked @endif><br>
-            <label for="cashier">Cashier</label>
-            <input type="radio" name="role" value="cashier" @if((isset($user->role) && $user->role ==
-            "cashier")||
-            old('role') == "cashier") checked @endif><br>
-            <label for="administrator">Administrator</label>
-            <input type="radio" name="role" value="administrator" @if((isset($user->role) && $user->role ==
-            "administrator") || old('role') == "administrator") checked @endif>
+            <div class="field">
+                <label class="label">Balance</label>
+                <div class="control has-icons-left">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-dollar-sign"></i>
+                    </span>
+                    <input type="number" step="0.01" name="balance" class="input" value="{{ isset($user->balance) ? number_format($user->balance, 2) : number_format(old('balance'), 2) }}">
+                </div>
+            </div>
 
-            <input type="password" name="password" class="form-control password_hidable" placeholder="Password"
-                autocomplete="new-password" readonly>
-            <input type="password" name="password_confirmation" class="form-control password_hidable"
-                placeholder="Confirm Password" autocomplete="new-password" readonly>
+            <div class="field">
+                <div class="control">
+                    <label class="label">Role</label>
+                    <label class="radio">
+                        <input type="radio" name="role" value="camper" @if((isset($user->role) && $user->role ==  "camper") || old('role') == "camper") checked @endif>
+                        Camper
+                    </label>
+                    <label class="radio">
+                    <input type="radio" name="role" value="cashier" @if((isset($user->role) && $user->role == "cashier") || old('role') == "cashier") checked @endif>
+                        Cashier
+                    </label>
+                    <label class="radio">
+                    <input type="radio" name="role" value="administrator" @if((isset($user->role) && $user->role == "administrator") || old('role') == "administrator") checked @endif>
+                        Administrator
+                    </label>
+                </div>
+            </div>
+
+            <label class="label">Password</label>
+            <div class="field">
+                <div class="control has-icons-left">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-lock"></i>
+                    </span>
+                    <input type="password" name="password" class="input password_hidable" placeholder="Password" autocomplete="new-password" readonly>
+                </div>
+            </div>
+            <div class="field">
+                <div class="control has-icons-left">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-lock"></i>
+                    </span>
+                    <input type="password" name="password_confirmation" class="input password_hidable" placeholder="Confirm Password" autocomplete="new-password" readonly>
+                </div>
+            </div>
     </div>
 
-    <div class="col-md-4">
+    <div class="column is-4">
         @include('includes.messages')
 
         <input type="hidden" name="editor_id" value="{{ Auth::user()->id }}">
 
         @foreach(SettingsController::getCategories() as $category)
-        <span>{{ ucfirst($category->value) }} Limit</span>
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <div class="input-group-text">$</div>
+            <div class="field">
+                <label class="label">{{ ucfirst($category->value) }} Limit</label>
+                <div class="control has-icons-left">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-dollar-sign"></i>
+                    </span>
+                    <input type="number" step="0.01" name="limit[{{ $category->value }}]" class="input" placeholder="Limit" value="{{ isset($user->id) ? number_format(UserLimitsController::findLimit($user->id, $category->value), 2) : '' }}">
+                </div>
+                <div class="control">
+                    <label class="label">
+                        <input type="radio" name="duration[{{ $category->value }}]" value="0" @if(isset($user->id) && UserLimitsController::findDuration($user->id, $category->value) == "day") checked @endif>
+                        Day
+                    </label>
+                    <label class="label"">
+                        <input type="radio" name="duration[{{ $category->value }}]" value="1" @if(isset($user->id) && UserLimitsController::findDuration($user->id, $category->value) == "week") checked @endif>
+                        Week
+                    </label>
+                </div>
             </div>
-            <input type="number" step="0.01" name="limit[{{ $category->value }}]" class="form-control"
-                placeholder="Limit"
-                value="{{ isset($user->id) ? UserLimitsController::findLimit($user->id, $category->value) : '' }}">
-        </div>
-        <input type="radio" name="duration[{{ $category->value }}]" value="0" @if(isset($user->id) &&
-        UserLimitsController::findDuration($user->id, $category->value) == "day") checked @endif>
-        <label for="day">Day</label>&nbsp;
-        <input type="radio" name="duration[{{ $category->value }}]" value="1" @if(isset($user->id) &&
-        UserLimitsController::findDuration($user->id, $category->value) == "week") checked @endif>
-        <label for="week">Week</label>
-        <br>
         @endforeach
     </div>
     </form>
-    <div class="col-md-2">
+    <div class="column is-2">
         <form>
-            <button type="submit" form="user_form" class="button is-success">Submit</button>
+            <div class="control">
+                <button class="button is-success" type="submit" form="user_form">
+                    <span class="icon is-small">
+                        <i class="fas fa-check"></i>
+                    </span>
+                    <span>Submit</span>
+                </button>
+            </div>
         </form>
         <br>
         @if(!is_null($user))
-        <form>
-            <a href="javascript:;" data-toggle="modal" onclick="deleteData()" data-target="#DeleteModal"
-                class="button is-danger">Delete</a>
-        </form>
+            <div class="control">
+                <form>
+                    <a class="button is-danger is-outlined" href="javascript:;" data-toggle="modal" onclick="deleteData()" data-target="#DeleteModal">
+                        <span>Delete</span>
+                        <span class="icon is-small">
+                            <i class="fas fa-times"></i>
+                        </span>
+                    </a>
+                </form>
+            </div>
         @endif
     </div>
 </div>
 <div id="DeleteModal" class="modal fade" role="dialog">
-    <div class="modal-dialog ">
+    <div class="modal-dialog">
         <form action="" id="deleteForm" method="get">
             <div class="modal-content">
                 <div class="modal-body">
@@ -98,11 +145,8 @@ if (!is_null($user) && $user->deleted) return redirect('/users')->with('error', 
                     <p class="text-center">Are you sure you want to delete this user?</p>
                 </div>
                 <div class="modal-footer">
-                    <center>
                         <button type="button" class="button is-success" data-dismiss="modal">Cancel</button>
-                        <button type="submit" name="" class="button is-danger" data-dismiss="modal"
-                            onclick="formSubmit()">Delete</button>
-                    </center>
+                        <button type="submit" name="" class="button is-danger" data-dismiss="modal" onclick="formSubmit()">Delete</button>
                 </div>
             </div>
         </form>
