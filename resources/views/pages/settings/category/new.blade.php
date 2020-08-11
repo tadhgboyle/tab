6a8754a752887436576c9cdb4ba90d222ba53b4d
Tadhg <tadhgsmboyle@gmail.com>
2020-08-11 00:05:25 -0700

    minor design changes

--- resources/views/pages/settings/category/new.blade.php
+++ resources/views/pages/settings/category/new.blade.php
@@ -1,20 +1,30 @@
 @extends('layouts.default')
 @section('content')
-<h2>Create Category</h2>
-<div class="row">
-    <div class="col-md-4"></div>
-    <div class="col-md-4">
+<h2 class="title has-text-weight-bold">Create Category</h2>
+<div class="columns box">
+    <div class="column"></div>
+    <div class="column">
         @include('includes.messages')
         <form action="/settings/category/new" method="POST">
             @csrf
             <input type="hidden" name="editor_id" value="{{ Auth::user()->id }}">
-            <span>Name<sup style="color: red">*</sup></span>
-            <input type="text" name="name" class="form-control" placeholder="Category Name" value={{ old('name') }}>
-            <br>
-            <button type="submit" class="button is-success">Submit</button>
+            <div class="field">
+                <label class="label">Name<sup style="color: red">*</sup></label>
+                <div class="control">
+                    <input type="text" name="name" class="input" placeholder="Category Name" value={{ old('name') }}>
+                </div>
+            </div>
+            <div class="control">
+                <button class="button is-success" type="submit">
+                    <span class="icon is-small">
+                        <i class="fas fa-save"></i>
+                    </span>
+                    <span>Submit</span>
+                </button>
+            </div>
         </form>
     </div>
-    <div class="col-md-4">
+    <div class="column">
     </div>
 </div>
 @stop
