d0bf4a022a88535412c23184324f5a6a37d03ddf
Tadhg <tadhgsmboyle@gmail.com>
2020-08-20 15:12:40 -0700

    update User validation handling & etc stuff

--- app/User.php
+++ app/User.php
@@ -15,14 +15,14 @@ class User extends Authenticatable
 
     // Find how much a user has spent in total. 
     // Does not factor in returned items/orders.
-    public static function findSpent($user)
+    public static function findSpent(User $user): float
     {
         return number_format(Transactions::where('purchaser_id', $user->id)->sum('total_price'), 2);
     }
 
     // Find how much a user has returned in total.
     // This will see if a whole order has been returned, or if not, check all items in an unreturned order.
-    public static function findReturned($user)
+    public static function findReturned(User $user): float
     {
         $returned = 0.00;
         $transactions = Transactions::where('purchaser_id', $user->id)->get();
@@ -48,7 +48,7 @@ class User extends Authenticatable
 
     // Find how much money a user owes. 
     // Taking their amount spent and subtracting the amount they have returned.
-    public static function findOwing($user)
+    public static function findOwing(User $user): float
     {
         return number_format(User::findSpent($user) - User::findReturned($user), 2);
     }
