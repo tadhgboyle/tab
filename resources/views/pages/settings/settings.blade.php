25729c5416f4f95343a48609630c4a78b7d5f488
Tadhg <tadhgsmboyle@gmail.com>
2020-09-07 21:01:15 -0700

    Loading gif on settings page

--- resources/views/pages/settings/settings.blade.php
+++ resources/views/pages/settings/settings.blade.php
@@ -58,31 +58,36 @@ $manage_categories = Roles::hasPermission(Auth::user()->role, 'settings_categori
         <div class="column is-3">
             <div class="box">
                 <h4 class="title has-text-weight-bold is-4">Categories</h4>
-                <table id="category_list">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach(SettingsController::getCategories() as $category)
-                        <tr>
-                            <td>
-                                <div>{{ ucfirst($category->value) }}</div>
-                            </td>
-                            <td>
-                                <div>
-                                    <form>
-                                        <input type="hidden" id="{{ $category->value }}" value="{{ $category->value }}">
-                                        <a href="javascript:;">Edit</a>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <div id="category_loading" align="center">
+                    <img src="{{ url('loader.gif') }}" alt="Loading..." class="loading-spinner" />
+                </div>
+                <div id="category_container" style="visibility: hidden;">
+                    <table id="category_list">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach(SettingsController::getCategories() as $category)
+                            <tr>
+                                <td>
+                                    <div>{{ ucfirst($category->value) }}</div>
+                                </td>
+                                <td>
+                                    <div>
+                                        <form>
+                                            <input type="hidden" id="{{ $category->value }}" value="{{ $category->value }}">
+                                            <a href="javascript:;">Edit</a>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
                 <br>
                 <a class="button is-success" href="{{ route('settings_categories_new') }}">
                     <span class="icon is-small">
@@ -99,42 +104,47 @@ $manage_categories = Roles::hasPermission(Auth::user()->role, 'settings_categori
     @if($manage_roles)
         <div class="column box is-4">
             <h4 class="title has-text-weight-bold is-4">Roles</h4>
-            <table id="role_list">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Staff</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody id="sortable">
-                    @foreach(Roles::getRoles('ASC') as $role)
-                        <tr data-id="{{ $role->id }}">
-                            <td>
-                                <div>{{ $role->name }}</div>
-                            </td>
-                            <td>
-                                <div>{!! $role->staff ? "<span class=\"tag is-success is-medium\">Yes</span>" : "<span class=\"tag is-danger is-medium\">No</span>" !!}</div>
-                            </td>
-                            <td>
-                                <div>
-                                @if (Roles::canInteract(Auth::user()->role, $role->id))
-                                    <a href="{{ route('settings_roles_edit', $role->id) }}">Edit</a>
-                                @else 
-                                    <div class="control">
-                                        <button class="button is-warning" disabled>
-                                            <span class="icon">
-                                                <i class="fas fa-lock"></i>
-                                            </span>
-                                        </button>
-                                    </div>
-                                @endif
-                                </div>
-                            </td>
+            <div id="role_loading" align="center">
+                <img src="{{ url('loader.gif') }}" alt="Loading..." class="loading-spinner" />
+            </div>
+            <div id="role_container" style="visibility: hidden;">
+                <table id="role_list">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Staff</th>
+                            <th></th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody id="sortable">
+                        @foreach(Roles::getRoles('ASC') as $role)
+                            <tr data-id="{{ $role->id }}">
+                                <td>
+                                    <div>{{ $role->name }}</div>
+                                </td>
+                                <td>
+                                    <div>{!! $role->staff ? "<span class=\"tag is-success is-medium\">Yes</span>" : "<span class=\"tag is-danger is-medium\">No</span>" !!}</div>
+                                </td>
+                                <td>
+                                    <div>
+                                    @if (Roles::canInteract(Auth::user()->role, $role->id))
+                                        <a href="{{ route('settings_roles_edit', $role->id) }}">Edit</a>
+                                    @else 
+                                        <div class="control">
+                                            <button class="button is-warning" disabled>
+                                                <span class="icon">z
+                                                    <i class="fas fa-lock"></i>
+                                                </span>
+                                            </button>
+                                        </div>
+                                    @endif
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
             <br>
             <a class="button is-success" href="{{ route('settings_roles_new') }}">
                 <span class="icon is-small">
@@ -213,6 +223,11 @@ $manage_categories = Roles::hasPermission(Auth::user()->role, 'settings_categori
                 });
             @endif
         @endif
+
+        $('#category_loading').hide();
+        $('#category_container').css('visibility', 'visible');
+        $('#role_loading').hide();
+        $('#role_container').css('visibility', 'visible');
     });
 </script>
 @stop
