58d51ad656f2e3df2ec22053b1f27cec945ae348
Tadhg <tadhgsmboyle@gmail.com>
2021-02-27 14:18:09 -0800

    create "page" helper function for navbar is-active class

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -4,11 +4,25 @@ if (! function_exists('hasPermission')) {
     /**
      * Check if the logged in user has specified permission
      *
-     * @param  $permission
+     * @param $permission
      * @return bool
      */
     function hasPermission($permission): bool
     {
         return Auth::user()->hasPermission($permission);
+    }
+}
+
+if (! function_exists('page')) {
+    /**
+     * Check if current page matches this navbar item and display "is-active" class if so
+     *
+     * @param $navbar_item
+     * @param $page
+     * @return bool
+     */
+    function page(string $current_page, ?string $page): string
+    {
+        return !is_null($page) && $page == $current_page ? ' is-active ' : '';
     }
 }
