46058c81a74d5fd161c929951204be74a422f6f4
Tadhg <tadhgsmboyle@gmail.com>
2021-02-27 14:27:07 -0800

    use Arr::random() instead of Collection->random() for footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <hr>
 <footer style="margin-bottom: 20px;">
     @auth
-    <p>{{ Illuminate\Database\Eloquent\Collection::make(['Welcome back', 'Greetings', 'Good day', 'Hello', 'Howdy', 'Bonjour', 'Hola', 'Long time no see', 'Salutations', 'Peek-a-boo', 'Ahoy', 'Top of the morning', 'G\'day'])->random() }}, <b>{{ Auth::user()->full_name }}</b> ({{ Auth::user()->role->name }})</p>
+    <p>{{ Arr::random(['Welcome back', 'Greetings', 'Good day', 'Hello', 'Howdy', 'Bonjour', 'Hola', 'Long time no see', 'Salutations', 'Peek-a-boo', 'Ahoy', 'Top of the morning', 'G\'day']) }}, <b>{{ Auth::user()->full_name }}</b> ({{ Auth::user()->role->name }})</p>
     @endauth
     <p><i>tabReborn</i> | Version: {{ env('APP_VERSION') }}.{{ trim(@exec('git log --pretty="%h" -n1 HEAD')) }}</p>
 </footer>
