55c033dc3a5e2d8f1b83c3936093aa7d2e3c42e2
Tadhg Boyle <Aberdeener@users.noreply.github.com>
2021-05-07 04:15:13 +0000

    Apply fixes from StyleCI

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,8 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\Product;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ProductFactory extends Factory
 {
