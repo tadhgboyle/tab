1282e587311b830d0d7a0a8848798382d1b56062
Tadhg Boyle <Aberdeener@users.noreply.github.com>
2021-06-10 23:14:51 +0000

    Apply fixes from StyleCI

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -11,20 +11,20 @@ abstract class TransactionService
     protected string $_message;
     protected Transaction $_transaction;
 
-    public final function getResult(): int
+    final public function getResult(): int
     {
         return $this->_result;
     }
 
-    public final function getMessage(): string
+    final public function getMessage(): string
     {
         return $this->_message;
     }
 
-    public final function getTransaction(): Transaction
+    final public function getTransaction(): Transaction
     {
         return $this->_transaction;
     }
 
-    abstract function redirect(): RedirectResponse;
+    abstract public function redirect(): RedirectResponse;
 }
