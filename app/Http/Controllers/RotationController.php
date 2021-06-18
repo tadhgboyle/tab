2b5bcc0d6c912bd2835fc13aec11438d2322eb1f
Tadhg <tadhgsmboyle@gmail.com>
2021-06-17 17:23:01 -0700

    misc Rotation work

--- app/Http/Controllers/RotationController.php
+++ app/Http/Controllers/RotationController.php
@@ -2,7 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RotationRequest;
+use App\Models\Rotation;
+
 class RotationController extends Controller
 {
-    //
+    public function form()
+    {
+        return view('pages.settings.rotations.form', [
+            'rotation' => Rotation::find(request()->route('id')),
+        ]);
+    }
+
+    public function new(RotationRequest $request)
+    {
+        $rotation = new Rotation();
+        $rotation->name = $request->name;
+        $rotation->start = $request->start;
+        $rotation->end = $request->end;
+        $rotation->save();
+
+        return redirect()->route('settings')->with('success', "Created new rotation {$request->name}.");
+    }
+
+    public function edit(RotationRequest $request)
+    {
+        $rotation = Category::find($request->rotation_id);
+
+        $rotation->update([
+            'name' => $request->name,
+            'start' => $request->start,
+            'end' => $request->end,
+        ]);
+
+        return redirect()->route('settings')->with('success', "Updated rotation {$request->name}.");
+    }
+
+    // TODO: fallback category logic similar to roles
+    public function delete(int $rotation_id)
+    {
+        $rotation = Rotation::find($rotation_id);
+
+        $rotation->delete();
+
+        return redirect()->route('settings')->with('success', "Deleted rotation {$rotation->name}.");
+    }
 }
--- resources/views/pages/settings/settings.blade.php
+++ resources/views/pages/settings/settings.blade.php
@@ -3,9 +3,9 @@
 <h2 class="title has-text-weight-bold">Settings</h2>
 <div class="columns">
     @permission('settings_general')
-    <div class="column is-3">
+    <div class="column">
         <div class="box">
-            <h4 class="title has-text-weight-bold is-4">General</h4>
+            <h4 class="title has-text-weight-bold is-4">Taxes</h4>
             @include('includes.messages')
             <form action="{{ route('settings_form') }}" id="settings" method="POST">
                 @csrf
@@ -43,10 +43,8 @@
     </div>
     @endpermission
 
-    <div class="column"></div>
-
     @permission('settings_categories_manage')
-    <div class="column is-4">
+    <div class="column">
         <div class="box">
             <h4 class="title has-text-weight-bold is-4">Categories</h4>
             <div id="category_loading" align="center">
@@ -88,60 +86,112 @@
         </div>
     </div>
     @endpermission
+</div>
 
-    <div class="column"></div>
-
+<div class="columns">
     @permission('settings_roles_manage')
-    <div class="column box is-4">
-        <h4 class="title has-text-weight-bold is-4">Roles</h4>
-        <div id="role_loading" align="center">
-            <img src="{{ url('img/loader.gif') }}" alt="Loading..." class="loading-spinner" />
+    <div class="column">
+        <div class="box">
+            <h4 class="title has-text-weight-bold is-4">Roles</h4>
+            <div id="role_loading" align="center">
+                <img src="{{ url('img/loader.gif') }}" alt="Loading..." class="loading-spinner" />
+            </div>
+            <div id="role_container" style="visibility: hidden;">
+                <table id="role_list">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Staff</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody id="sortable">
+                        @foreach($roles as $role)
+                        <tr data-id="{{ $role->id }}">
+                            <td>
+                                <div>{{ $role->name }}</div>
+                            </td>
+                            <td>
+                                <div>{!! $role->staff ? "<span class=\"tag is-success is-medium\">Yes</span>" : "<span class=\"tag is-danger is-medium\">No</span>" !!}</div>
+                            </td>
+                            <td>
+                                <div>
+                                    @if (Auth::user()->role->canInteract($role))
+                                        <a href="{{ route('settings_roles_edit', $role->id) }}">Edit</a>
+                                    @else
+                                        <div class="control">
+                                            <button class="button is-warning" disabled>
+                                                <span class="icon">
+                                                    <i class="fas fa-lock"></i>
+                                                </span>
+                                            </button>
+                                        </div>
+                                    @endif
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <br>
+            <a class="button is-success" href="{{ route('settings_roles_new') }}">
+                <span class="icon is-small">
+                    <i class="fas fa-plus"></i>
+                </span>
+                <span>New</span>
+            </a>
         </div>
-        <div id="role_container" style="visibility: hidden;">
-            <table id="role_list">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Staff</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody id="sortable">
-                    @foreach($roles as $role)
-                    <tr data-id="{{ $role->id }}">
-                        <td>
-                            <div>{{ $role->name }}</div>
-                        </td>
-                        <td>
-                            <div>{!! $role->staff ? "<span class=\"tag is-success is-medium\">Yes</span>" : "<span class=\"tag is-danger is-medium\">No</span>" !!}</div>
-                        </td>
-                        <td>
-                            <div>
-                                @if (Auth::user()->role->canInteract($role))
-                                    <a href="{{ route('settings_roles_edit', $role->id) }}">Edit</a>
-                                @else
-                                    <div class="control">
-                                        <button class="button is-warning" disabled>
-                                            <span class="icon">
-                                                <i class="fas fa-lock"></i>
-                                            </span>
-                                        </button>
-                                    </div>
-                                @endif
-                            </div>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+    </div>
+    @endpermission
+
+    @permission('settings_rotations_manage')
+    <div class="column">
+        <div class="box">
+            <h4 class="title has-text-weight-bold is-4">Rotations</h4>
+            <div id="rotation_loading" align="center">
+                <img src="{{ url('img/loader.gif') }}" alt="Loading..." class="loading-spinner" />
+            </div>
+            <div id="rotation_container" style="visibility: hidden;">
+                <table id="rotation_list">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Start</th>
+                            <th>End</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody id="sortable">
+                        @foreach($rotations as $rotation)
+                        <tr data-id="{{ $rotation->id }}">
+                            <td>
+                                <div>{{ $rotation->name }}</div>
+                            </td>
+                            <td>
+                                <div>{{ $rotation->start->format('M jS Y') }}</div>
+                            </td>
+                            <td>
+                                <div>{{ $rotation->end->format('M jS Y') }}</div>
+                            </td>
+                            <td>
+                                <div>
+                                    <a href="{{ route('settings_rotations_edit', $rotation->id) }}">Edit</a>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <br>
+            <a class="button is-success" href="{{ route('settings_rotations_new') }}">
+                <span class="icon is-small">
+                    <i class="fas fa-plus"></i>
+                </span>
+                <span>New</span>
+            </a>
         </div>
-        <br>
-        <a class="button is-success" href="{{ route('settings_roles_new') }}">
-            <span class="icon is-small">
-                <i class="fas fa-plus"></i>
-            </span>
-            <span>New</span>
-        </a>
     </div>
     @endpermission
 </div>
@@ -212,10 +262,28 @@
             @endif
         @endpermission
 
+        @permission('settings_rotations_manage')
+            $('#rotation_list').DataTable({
+                "order": [],
+                "paging": false,
+                "searching": false,
+                "scrollY": "49vh",
+                "scrollCollapse": true,
+                "bInfo": false,
+                "columnDefs": [{
+                    "orderable": false,
+                    "searchable": false,
+                    "targets": [1, 2]
+                }]
+            });
+        @endpermission
+
         $('#category_loading').hide();
         $('#category_container').css('visibility', 'visible');
         $('#role_loading').hide();
         $('#role_container').css('visibility', 'visible');
+        $('#rotation_loading').hide();
+        $('#rotation_container').css('visibility', 'visible');
     });
 </script>
 @stop
