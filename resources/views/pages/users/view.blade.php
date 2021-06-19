9f0e6ebd8d0d8bb7670eca3b11c24d8389dbe7cc
Tadhg <tadhgsmboyle@gmail.com>
2021-06-19 10:30:55 -0700

    change layout of user viewing page

--- resources/views/pages/users/view.blade.php
+++ resources/views/pages/users/view.blade.php
@@ -18,182 +18,192 @@
 <div class="columns" id="table_container" style="visibility: hidden;">
 
     <div class="column">
-        <div class="box">
-            <h4 class="title has-text-weight-bold is-4">Order History</h4>
-            <table id="order_list">
-                <thead>
-                    <th>Time</th>
-                    <th>Cashier</th>
-                    <th>Price</th>
-                    <th>Status</th>
-                    @permission('orders_view')
-                    <th></th>
-                    @endpermission
-                </thead>
-                <tbody>
-                    @foreach($transactions as $transaction)
-                    <tr>
-                        <td>
-                            <div>{{ $transaction->created_at->format('M jS Y h:ia') }}</div>
-                        </td>
-                        <td>
-                            <div>{{ $transaction->cashier->full_name }}</div>
-                        </td>
-                        <td>
-                            <div>${{ number_format($transaction->total_price, 2) }}</div>
-                        </td>
-                        <td>
-                            <div>
-                                @switch($transaction->getReturnStatus())
-                                    @case(0)
-                                        <span class="tag is-success is-medium">Normal</span>
-                                    @break
-                                    @case(1)
-                                        <span class="tag is-danger is-medium">Returned</span>
-                                    @break
-                                    @case(2)
-                                        <span class="tag is-warning is-medium">Semi Returned</span>
-                                    @break
-                                @endswitch
-                            </div>
-                        </td>
-                        @permission('orders_view')
-                        <td>
-                            <div><a href="{{ route('orders_view', $transaction->id) }}">View</a></div>
-                        </td>
-                        @endpermission
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-
-    <div class="column is-5">
         <div class="columns is-multiline">
-            <div class="column is-12 box">
-                <h4 class="title has-text-weight-bold is-4">Category Limits</h4>
-                <table id="category_list">
-                    <thead>
-                        <tr>
-                            <th>Category</th>
-                            <th>Limit</th>
-                            <th>Spent</th>
-                            <th>Remaining</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($categories as $category)
-                            <tr>
-                                <td>
-                                    <div>{{ $category['name'] }}</div>
-                                </td>
-                                <td>
-                                    <div>{!! $category['limit'] == -1 ? "<i>Unlimited</i>" : "$" . number_format($category['limit'], 2) . "/" . $category['duration'] !!}</div>
-                                </td>
-                                <td>
-                                    <div>${{ number_format($category['spent'], 2) }}</div>
-                                </td>
-                                <td>
-                                    <div>{!! $category['limit'] == -1 ? "<i>Unlimited</i>" : "$" . number_format($category['limit'] - $category['spent'], 2) !!}</div>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <div class="column is-12 box">
-                <h4 class="title has-text-weight-bold is-4">Rotations</h4>
-                <table id="rotation_list">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Start</th>
-                            <th>End</th>
+            <div class="column">
+                <div class="box">
+                    <h4 class="title has-text-weight-bold is-4">Order History</h4>
+                    <table id="order_list">
+                        <thead>
+                            <th>Time</th>
+                            <th>Cashier</th>
+                            <th>Price</th>
                             <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($rotations as $rotation)
+                            @permission('orders_view')
+                            <th></th>
+                            @endpermission
+                        </thead>
+                        <tbody>
+                            @foreach($transactions as $transaction)
                             <tr>
                                 <td>
-                                    <div>{{ $rotation->name }}</div>
+                                    <div>{{ $transaction->created_at->format('M jS Y h:ia') }}</div>
                                 </td>
                                 <td>
-                                    <div>{{ $rotation->start->format('M jS Y h:ia') }}</div>
+                                    <div>{{ $transaction->cashier->full_name }}</div>
                                 </td>
                                 <td>
-                                    <div>{{ $rotation->end->format('M jS Y h:ia') }}</div>
+                                    <div>${{ number_format($transaction->total_price, 2) }}</div>
                                 </td>
                                 <td>
                                     <div>
-                                        @switch($rotation->getStatus())
+                                        @switch($transaction->getReturnStatus())
                                             @case(0)
-                                                <span class="tag is-success is-medium">Present</span>
+                                                <span class="tag is-success is-medium">Normal</span>
                                             @break
                                             @case(1)
-                                                <span class="tag is-warning is-medium">Future</span>
+                                                <span class="tag is-danger is-medium">Returned</span>
                                             @break
                                             @case(2)
-                                                <span class="tag is-warning is-medium">Past</span>
+                                                <span class="tag is-warning is-medium">Semi Returned</span>
+                                            @break
+                                        @endswitch
+                                    </div>
+                                </td>
+                                @permission('orders_view')
+                                <td>
+                                    <div><a href="{{ route('orders_view', $transaction->id) }}">View</a></div>
+                                </td>
+                                @endpermission
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="column">
+                <div class="box">
+                    <h4 class="title has-text-weight-bold is-4">Activity History</h4>
+                    <table id="activity_list">
+                        <thead>
+                            <tr>
+                                <th>Time</th>
+                                <th>Cashier</th>
+                                <th>Activity</th>
+                                <th>Price</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($activity_transactions as $transaction)
+                            <tr>
+                                <td>
+                                    <div>{{ $transaction['created_at']->format('M jS Y h:ia') }}</div>
+                                </td>
+                                <td>
+                                    <div>{{ $transaction['cashier']->full_name }}</div>
+                                </td>
+                                <td>
+                                    <div>
+                                        @permission('activities_view')
+                                            <a href="{{ route('activities_view', $transaction['activity']->id) }}">{{ $transaction['activity']->name }}</a>
+                                        @else
+                                            {{ $transaction['activity']->name }}
+                                        @endpermission
+                                    </div>
+                                </td>
+                                <td>
+                                    <div>{!! $transaction['price'] > 0 ? '$' . number_format($transaction['price'], 2) : '<i>Free</i>' !!}</div>
+                                </td>
+                                <td>
+                                    <div>
+                                        @switch($transaction['returned'])
+                                            @case(0)
+                                                <span class="tag is-success is-medium">Normal</span>
+                                            @break
+                                            @case(1)
+                                                <span class="tag is-danger is-medium">Returned</span>
                                             @break
                                         @endswitch
                                     </div>
                                 </td>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div class="column box">
-                <h4 class="title has-text-weight-bold is-4">Activity History</h4>
-                <table id="activity_list">
-                    <thead>
-                        <tr>
-                            <th>Time</th>
-                            <th>Cashier</th>
-                            <th>Activity</th>
-                            <th>Price</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($activity_transactions as $transaction)
-                        <tr>
-                            <td>
-                                <div>{{ $transaction['created_at']->format('M jS Y h:ia') }}</div>
-                            </td>
-                            <td>
-                                <div>{{ $transaction['cashier']->full_name }}</div>
-                            </td>
-                            <td>
-                                <div>
-                                    @permission('activities_view')
-                                        <a href="{{ route('activities_view', $transaction['activity']->id) }}">{{ $transaction['activity']->name }}</a>
-                                    @else
-                                        {{ $transaction['activity']->name }}
-                                    @endpermission
-                                </div>
-                            </td>
-                            <td>
-                                <div>{!! $transaction['price'] > 0 ? '$' . number_format($transaction['price'], 2) : '<i>Free</i>' !!}</div>
-                            </td>
-                            <td>
-                                <div>
-                                    @switch($transaction['returned'])
-                                        @case(0)
-                                            <span class="tag is-success is-medium">Normal</span>
-                                        @break
-                                        @case(1)
-                                            <span class="tag is-danger is-medium">Returned</span>
-                                        @break
-                                    @endswitch
-                                </div>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="columns is-multiline">
+            <div class="column">
+                <div class="box">
+                    <h4 class="title has-text-weight-bold is-4">Category Limits</h4>
+                    <table id="category_list">
+                        <thead>
+                            <tr>
+                                <th>Category</th>
+                                <th>Limit</th>
+                                <th>Spent</th>
+                                <th>Remaining</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($categories as $category)
+                                <tr>
+                                    <td>
+                                        <div>{{ $category['name'] }}</div>
+                                    </td>
+                                    <td>
+                                        <div>{!! $category['limit'] == -1 ? "<i>Unlimited</i>" : "$" . number_format($category['limit'], 2) . "/" . $category['duration'] !!}</div>
+                                    </td>
+                                    <td>
+                                        <div>${{ number_format($category['spent'], 2) }}</div>
+                                    </td>
+                                    <td>
+                                        <div>{!! $category['limit'] == -1 ? "<i>Unlimited</i>" : "$" . number_format($category['limit'] - $category['spent'], 2) !!}</div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="column">
+                <div class="box">
+                    <h4 class="title has-text-weight-bold is-4">Rotations</h4>
+                    <table id="rotation_list">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Start</th>
+                                <th>End</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($rotations as $rotation)
+                                <tr>
+                                    <td>
+                                        <div>{{ $rotation->name }}</div>
+                                    </td>
+                                    <td>
+                                        <div>{{ $rotation->start->format('M jS Y h:ia') }}</div>
+                                    </td>
+                                    <td>
+                                        <div>{{ $rotation->end->format('M jS Y h:ia') }}</div>
+                                    </td>
+                                    <td>
+                                        <div>
+                                            @switch($rotation->getStatus())
+                                                @case(0)
+                                                    <span class="tag is-success is-medium">Present</span>
+                                                @break
+                                                @case(1)
+                                                    <span class="tag is-warning is-medium">Future</span>
+                                                @break
+                                                @case(2)
+                                                    <span class="tag is-warning is-medium">Past</span>
+                                                @break
+                                            @endswitch
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
