8b0ea5da5f256a423805a0b2bea1447164155749
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-11-04 23:03:07 -0600

    Login controller test

---
+++ tests/Feature/LoginControllerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LoginControllerTest extends TestCase {
+
+    use RefreshDatabase;
+
+    private User $_user;
+
+    public function setUp(): void {
+        parent::setUp();
+
+        $role = Role::factory()->create();
+        $this->_user = User::factory()->create([
+            'role_id' => $role->id,
+            'password' => bcrypt('password'),
+        ]);
+    }
+
+    public function testCanViewLoginPageWhenLoggedOut(): void
+    {
+        $this->get(route('login'))
+            ->assertOk()
+            ->assertViewIs('pages.login');
+    }
+
+    public function testCanLoginWithValidCredentials(): void {
+        $this
+            ->post(route('login_auth'), [
+                'username' => $this->_user->username,
+                'password' => 'password',
+            ])
+            ->assertRedirect(route('index'));
+    }
+
+    public function testCannotLoginWithInvalidCredentials(): void {
+        $this
+            ->post(route('login_auth'), [
+                'username' => $this->_user->username,
+                'password' => 'invalid_password',
+            ])
+            ->assertRedirect(route('login'))
+            ->assertSessionHas('error', 'Invalid credentials. Please try again.');
+    }
+
+    public function testCanLogoutIfLoggedIn(): void {
+        $this
+            ->actingAs($this->_user)
+            ->get(route('logout'))
+            ->assertRedirect(route('index'));
+
+        $this->assertGuest();
+    }
+}
