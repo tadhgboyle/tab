56cca6328caa7d0775f6c1d233ab9ab05f7e17dc
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-11-06 16:26:47 -0700

    Fix phpstan

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -124,7 +124,7 @@ class ActivityController extends Controller
                 '<tr>' .
                     '<td>' . $user->full_name . '</td>' .
                     '<td>$' . number_format($user->balance, 2) . '</td>' .
-                    (($user->balance < $activity->getPrice() || $activity->isAttending($user))
+                    (($user->balance < $activity->getPriceAfterTax() || $activity->isAttending($user))
                         ? '<td><button class="button is-success is-small" disabled>Add</button></td>'
                         : '<td><a href="' . route('activities_user_add', [$activity->id, $user->id]) . '" class="button is-success is-small">Add</a></td>') .
                 '</tr>';
