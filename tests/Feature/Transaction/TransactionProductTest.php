95640ed4b984f7003f176120a65fdbe6acc2c634
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-11-11 19:08:39 -0700

    scaffold more tests

---
+++ tests/Feature/Transaction/TransactionProductTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Transaction;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TransactionProductTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testPriceIsCastedToMoneyObject(): void
+    {
+
+    }
+
+    public function testHasTransaction(): void
+    {
+
+    }
+
+    public function testHasProduct(): void
+    {
+
+    }
+
+    public function testHasCategory(): void
+    {
+
+    }
+
+    public function testCanCreateFromProduct(): void
+    {
+
+    }
+}
---
+++ tests/Feature/Transaction/TransactionTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature\Transaction;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TransactionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testTotalPriceCastedToMoneyObject(): void
+    {
+
+    }
+
+    public function testHasPurchaser(): void
+    {
+
+    }
+
+    public function testHasCashier(): void
+    {
+
+    }
+
+    public function testHasRotation(): void
+    {
+
+    }
+
+    public function testHasProducts(): void
+    {
+
+    }
+
+    public function testGetReturnedTotalIsFullPriceIfFullyReturned(): void
+    {
+
+    }
+
+    public function testGetReturnedTotalIsZeroIfNotReturned(): void
+    {
+
+    }
+
+    public function testGetReturnedTotalIsPartialPriceIfPartiallyReturned(): void
+    {
+
+    }
+
+    public function testIsReturnedIsTrueIfFullyReturned(): void
+    {
+
+    }
+
+    public function testGetReturnStatusIsFullyReturnedIfReturned(): void
+    {
+
+    }
+
+    public function testGetReturnStatusIsNotReturnedIfNotReturned(): void
+    {
+
+    }
+
+    public function testGetReturnStatusIsPartiallyReturnedIfPartiallyReturned(): void
+    {
+
+    }
+}
