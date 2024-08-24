7781d059ff68c5ca391a83cfdb66c73b7598f230
Tadhg Boyle <tadhgsmboyle@gmail.com>
2024-08-24 15:57:10 -0700

    remove fontawesome as npm dependency

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="{{ url('css/app.css') }}">
     <link rel="stylesheet" href="{{ url('css/dataTables-1.10.21.min.css') }}">
     <link rel="stylesheet" href="{{ url('css/styles.css') }}" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
 
     <script src="{{ url('js/app.js') }}"></script>
 </head>
