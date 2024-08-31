860b34416acece311919074942c1d94e5ea72113
Tadhg Boyle <tadhgsmboyle@gmail.com>
2024-08-31 16:58:35 -0700

    fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,5 +4,4 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import 'bulma';
-import 'jquery-ui/ui/widgets/sortable';
-import 'datatables.net';
+
