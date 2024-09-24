25e6d1c23d60b3c3181d1d4bb36def0497348460
Tadhg Boyle <tadhgsmboyle@gmail.com>
2024-09-23 18:39:57 -0700

    fix long descriptions

--- resources/views/components/detail-card-item.blade.php
+++ resources/views/components/detail-card-item.blade.php
@@ -1,4 +1,4 @@
-<div class="py-3 flex justify-between items-center">
-    <dt class="text-sm font-semibold text-gray-950">{!! $label !!}</dt>
-    <dd class="text-sm text-gray-950">{!! $value ?? $slot !!}</dd>
+<div class="py-3 flex justify-between items-center text-sm text-gray-950">
+    <dt class="font-semibold">{!! $label !!}</dt>
+    <dd class="text-right">{!! $value ?? $slot !!}</dd>
 </div>
